b53ba88d12c4bc041eceba924b95c9a19b926b97
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-02-26 18:47:13 +0500

    Update wp-comments-post.php

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -10,7 +10,6 @@ if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) {
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
 		$protocol = 'HTTP/1.0';
 	}
-
 	header('Allow: POST');
 	header("$protocol 405 Method Not Allowed");
 	header('Content-Type: text/');
